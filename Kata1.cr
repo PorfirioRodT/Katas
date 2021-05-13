862656ae514c58ffc126876cc2cf4d3a91a5025f
PorfirioRodT <porfiriorodt@gmail.com>
2021-05-12 23:43:57 -0400

    Intento 1 Palindromo en Crystal Lang

--- Kata1.cr
+++ Kata1.cr
@@ -1,1 +1,16 @@
-" " 
+palindromes = ["ana", "oso", "true", "anita lava la tina", "madre"]
+
+def palindrome(p)
+  standar = String(p).lower().replace(" ", "")
+  return standar == standar[-1]
+end 
+
+name = ""
+
+if name == "ok"
+  loop do
+    resultado = palindrome(p)
+    puts(resultados)
+  break if palindrome != true
+  end
+end
