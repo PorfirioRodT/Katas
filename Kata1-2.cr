b729dffa7ded9f4803d9e5bbd8ca9cbe2b759930
PorfirioRodT <porfiriorodt@gmail.com>
2021-05-13 22:20:41 -0400

    Intento numero 2 de los palindromos en Crystal Language y codigo realizado en el video

--- Kata1-2.cr
+++ Kata1-2.cr
@@ -1,1 +1,26 @@
-""  
+def isPalindrome(value)
+  
+  result = true 
+  i = 0
+  
+  while(value.size - 1).downto(0)
+    
+    if value[i] != value[value.size - 1 - i]
+      result = false
+      break
+    end
+  end 
+    
+    return result
+  
+  	puts result
+    
+end
+  
+def palindromeWord()
+    
+   puts "La palabra 'ana' = #{isPalindrome("ana")}"
+    
+end
+     
+palindromeWord()
