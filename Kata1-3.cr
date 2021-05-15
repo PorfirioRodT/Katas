b13a4eb68f0318df37408ec02bde794d6eb3a2a3
PorfirioRodT <porfiriorodt@gmail.com>
2021-05-14 21:45:09 -0400

    Ver si una palabra es palindromo (completado)

--- Kata1-3.cr
+++ Kata1-3.cr
@@ -1,3 +1,24 @@
 
+def isPalindrome(word)
+
+    if word.reverse == word
+        
+        puts "La palabra #{word} es un palindromo"
+    
+    else
+
+        puts "La palabra #{word} no es un palindromo"
+    
+    end
+
+end
+
+puts isPalindrome("hola")
+puts isPalindrome("ana")
+puts isPalindrome("oso")
+puts isPalindrome("perro")
+puts isPalindrome("12321")
+
+
 
 
